599d7830b242e1faeb0fcf0446fa69bdae78bd80
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-11 16:03:06 +0200

    second solution, second folder

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -9,7 +9,10 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+let number= 17;
+let text= 'fiacco';
+let bool= true;
+console.log(number, text, bool);
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
